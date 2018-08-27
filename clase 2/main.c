5808dbc423b01424b72b7afedd7e638e5aab6f77
alancama <42609039+alancama@users.noreply.github.com>
2018-08-27 12:29:34 -0300

    Add files via upload

---
+++ clase 2/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int num;
+    int i;
+    float media;
+    int suma=0;
+
+    for(i=0;i<5;i++)
+        {
+           printf("\n ingrese un numero ");
+           scanf("%d",&num);
+
+           suma=suma+num;
+        }
+        media=suma/5;
+
+        printf("la media es :%.2f",media);*/
+/*-------------------------------------------------------------------------------------------------------*/
+
+        /*int num1,num2;
+        int suma;
+
+        printf("\n ingrese un numero");
+        scanf("%d",&num1);
+        printf("\n ingrese otro numero");
+        scanf("%d",&num2);
+
+        suma=num1 + num2;
+
+        printf("la suma es :%d",suma);*/
+
+ /*-------------------------------------------------------------------------------------------------------------*/
+
+
+
+      int num1=2;
+      int num2=2;
+
+      int alCuadrado;
+
+      int multiplicacion;
+
+      multiplicacion=num1*num2;
+
+      printf("el producto es :%d",multiplicacion);
+
+      system("cls");
+
+      alCuadrado=num1
+
+
+
+
+
+    return 0;
+}
